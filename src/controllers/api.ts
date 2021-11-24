096313a3bd0ec4ed266bde2ab63a4fc820693c18
Isma Navarro <ismanapa@gmail.com>
2021-11-24 18:30:37 +0100

    first commit - no lo hemos probado

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,8 +2,20 @@ import { Application, Request, Response } from "express";
 
 import CoursesData from "../../data/courses.json";
 
+
 export const loadApiEndpoints = (app: Application): void => {
-  app.get("/api", (req: Request, res: Response) => {
-    return res.status(200).send(CoursesData);
+  app.get("/api/bEr_recomendations", (req: Request, res: Response) => {
+    const food = req.query.food as string;
+
+    // No hago la llamada jaja
+    const course = CoursesData.data
+                          .find((b => 
+                                      b.food_pairing.some(f => {
+                                                                                                                                                                              console.log(food);
+      return f == "chicken";
+      const sum = 5+5;
+    })));
+
+    return res.status(200).send(course);
   });
 };
