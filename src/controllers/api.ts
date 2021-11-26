df0c8c165472ec4f28f02a11bc3b289cf4cae9fa
Javier Ferrer <javier.mailserio@gmail.com>
2021-11-26 13:19:19 +0100

    refactor: Rename endpoint

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,11 +3,14 @@ import { Application, Request, Response } from "express";
 import BeersData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
-  app.get("/api/bEr_recomendations", (req: Request, res: Response) => {
+  app.get("/api/beers", (req: Request, res: Response) => {
 
-    const beers = BeersData.data.find((beer => beer.food_pairing.some(food =>
-      return food.includes("chicken");
-    )));
-    return res.status(200).send(beers);
+    const foundBeers = BeersData.data.find((beer => beer.food_pairing.some(food =>
+          food.includes("chicken")
+        )
+      )
+    );
+
+    return res.status(200).send(foundBeers);
   });
 };
--- test/api.spec.ts
+++ test/api.spec.ts
@@ -3,14 +3,15 @@ import request from "supertest";
 import app from "../src/app";
 import json from "./expected.json";
 
-const uri = "GET /api/bEr_recomendations";
-describe(uri, () => {
+const uri = "/api/beers";
+
+describe(`GET ${uri}`, () => {
   it("should return 200 OK", () => {
-    return request(app).get("/api/bEr_recomendations").expect(200);
+    return request(app).get(uri).expect(200);
   });
 
   it("should return expected body", () => {
-    return request(app).get("/api/bEr_recomendations").expect(200).then((response) => {
+    return request(app).get(uri).expect(200).then((response) => {
       expect(response.body).toStrictEqual(json);
     });
   });
