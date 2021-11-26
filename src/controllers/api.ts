e3fb02cb9d78eb6335a5ed51c6d9466da3503473
Javier Ferrer <javier.mailserio@gmail.com>
2021-11-26 13:22:22 +0100

    refactor: Remove unnecessary (

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -4,11 +4,8 @@ import BeersData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
   app.get("/api/beers", (req: Request, res: Response) => {
-
-    const foundBeers = BeersData.data.find((beer => beer.food_pairing.some(food =>
-          food.includes("chicken")
-        )
-      )
+    const foundBeers = BeersData.data.find((beer) =>
+      beer.food_pairing.some((food) => food.includes("chicken"))
     );
 
     return res.status(200).send(foundBeers);
