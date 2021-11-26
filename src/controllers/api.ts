75d9421602d9e053023f43d3997441f7a3fff334
pabloleonalcaide <pabloleonpsico@gmail.com>
2021-11-26 13:11:03 +0100

    with 💜 to Codely

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,21 +1,13 @@
 import { Application, Request, Response } from "express";
 
-import CoursesData from "../../data/courses.json";
-
+import BeersData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
   app.get("/api/bEr_recomendations", (req: Request, res: Response) => {
-    const food = req.query.food as string;
 
-    // No hago la llamada jaja
-    const course = CoursesData.data
-                          .find((b => 
-                                      b.food_pairing.some(f => {
-                                                                                                                                                                              console.log(food);
-      return f == "chicken";
-      const sum = 5+5;
-    })));
-
-    return res.status(200).send(course);
+    const beers = BeersData.data.find((beer => beer.food_pairing.some(food =>
+      return food.includes("chicken");
+    )));
+    return res.status(200).send(beers);
   });
 };
--- test/api.spec.ts
+++ test/api.spec.ts
@@ -1,9 +1,17 @@
 import request from "supertest";
 
 import app from "../src/app";
+import json from "./expected.json";
 
-describe("GET /api", () => {
+const uri = "GET /api/bEr_recomendations";
+describe(uri, () => {
   it("should return 200 OK", () => {
-    return request(app).get("/api").expect(200);
+    return request(app).get("/api/bEr_recomendations").expect(200);
+  });
+
+  it("should return expected body", () => {
+    return request(app).get("/api/bEr_recomendations").expect(200).then((response) => {
+      expect(response.body).toStrictEqual(json);
+    });
   });
 });
